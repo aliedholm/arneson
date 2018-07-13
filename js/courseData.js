a307174af016c5e027f9011c25f899017a1c20ca
aliedholm <aliedholm@gmail.com>
2018-07-13 11:54:37 -0700

    built parse and sort function for the courses site. It currently sorts entries by year, but not by quarter within that year

---
+++ js/courseData.js
@@ -0,0 +1,56 @@
+var courses = [
+	{
+		"title" : "Well-Being & its Political Significance",
+		"courseNum" : "267",
+		"courseCode" : "Philosophy",
+		"quarter" : "Spring",
+		"year" : "2015",
+		"filename" : "phil267sp15.pdf"
+	},
+	{
+		"title" : "Theories of Distributive Justice",
+		"courseNum" : "202",
+		"courseCode" : "Philosophy",
+		"quarter" : "Spring",
+		"year" : "2014",
+		"filename" : "na"
+	},
+	{
+		"title" : "Intro to Philosophy",
+		"courseNum" : "1",
+		"courseCode" : "Philosophy",
+		"quarter" : "Fall",
+		"year" : "2016",
+		"filename" : "phil1fa16.pdf"
+	},
+		{
+		"title" : "Intro to Social Theory",
+		"courseNum" : "12",
+		"courseCode" : "Philosophy",
+		"quarter" : "Winter",
+		"year" : "2004",
+		"filename" : "phil12wi04.pdf"
+	},
+		{
+		"title" : "Intro to Logic",
+		"courseNum" : "10",
+		"courseCode" : "Philosophy",
+		"quarter" : "Fall",
+		"year" : "2011",
+		"filename" : "phil10fa11.pdf"
+	},
+
+	{
+		"title" : "Ethical Theory",
+		"courseNum" : "160",
+		"courseCode" : "Philosophy",
+		"quarter" : "Spring",
+		"year" : "2014",
+		"filename" : "phil160sp14.pdf"
+	}
+
+
+]
+
+
+
---
+++ js/courses.js
@@ -0,0 +1,78 @@
+//creating the data array for all the finalized data
+var upperCourses = [];
+var lowerCourses = [];
+var gradCourses = [];
+var nonNumCourses = [];
+//creating the arrays for each main section of the page
+var upper = [];
+var lower = [];
+var grad = [];
+//storting the contention section from html as a variable
+var upperSection = document.getElementById('upper-div');
+var lowerSection = document.getElementById('lower-div');
+var graduateSection = document.getElementById('graduate');
+//varibles for the frames that will hold the data
+var upperFrame = '';
+var lowerFrame = '';
+var gradFrame = '';
+
+//function to parse the main data array into sections by upper, lower and grad
+function sectionate(courses) {
+	for (var i=0; i < courses.length; i++) {
+		if (courses[i].courseNum < 100) {
+			lowerCourses.push(courses[i]);
+		}
+		else if ((courses[i].courseNum >= 100) && (courses[i].courseNum < 200)) {
+			upperCourses.push(courses[i]);
+		}
+		else if (courses[i].courseNum >= 200) {
+			gradCourses.push(courses[i]);
+		}
+		else {
+			noNumCourses.push(courses[i]);
+		}
+	}
+}
+
+//run the sectionate function
+sectionate(courses);
+
+//function to sort the sections into chronological order by year
+function compare(a, b) {
+	if (a.year < b.year) {
+		return 1;
+	}
+	if (a.year > b.year) {
+		return -1;
+	}
+	return 0;
+}
+
+//run the sort by year function
+gradCourses.sort(compare);	
+upperCourses.sort(compare);	
+lowerCourses.sort(compare);	
+
+//take the courses and create the html to be displayed by it by section
+function createHTML (courseSet) {
+	var frame = '';
+	for (var j=0; j < courseSet.length;	 j ++) {
+		if (courseSet[j].filename != "na") {
+			frame += "<li class='list-group-item body-text'>" + courseSet[j].courseCode + " " + courseSet[j].courseNum + " - " + courseSet[j].title + " (" + courseSet[j].quarter + " " + courseSet[j].year + ") " + "<a href='documents/syllabi/" + courseSet[j].filename + "''>" + "[syllabus]" + "</a></li>";
+		}
+		else {
+		frame += "<li class='list-group-item body-text'>" + courseSet[j].courseCode + " " + courseSet[j].courseNum + " - " + courseSet[j].title + " (" + courseSet[j].quarter + " " + courseSet[j].year + ") " + "</li>";
+		}
+	}
+	return frame;
+}
+
+// build each section
+gradFrame = createHTML(gradCourses);
+upperFrame = createHTML(upperCourses);
+lowerFrame = createHTML(lowerCourses);
+
+// write each section to the page
+graduateSection.innerHTML = gradFrame;
+upperSection.innerHTML = upperFrame;
+lowerSection.innerHTML = lowerFrame;
