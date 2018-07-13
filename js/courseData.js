c4f825c559122937bd2cb8da14fa12b2ce6ae7aa
aliedholm <aliedholm@gmail.com>
2018-07-13 12:07:49 -0700

    added duplicate fake course data to test page format while more entries are present. Added anchored links to the sub nav on the courses page

--- js/courseData.js
+++ js/courseData.js
@@ -31,6 +31,22 @@ var courses = [
 		"year" : "2004",
 		"filename" : "phil12wi04.pdf"
 	},
+	{
+		"title" : "Philosophy of Social Justice",
+		"courseNum" : "170",
+		"courseCode" : "Philosophy",
+		"quarter" : "Spring",
+		"year" : "2017",
+		"filename" : "na"
+	},
+	{
+		"title" : "Intro to Politics",
+		"courseNum" : "17",
+		"courseCode" : "Philosophy",
+		"quarter" : "Spring",
+		"year" : "2001",
+		"filename" : "na"
+	},
 		{
 		"title" : "Intro to Logic",
 		"courseNum" : "10",
@@ -39,7 +55,174 @@ var courses = [
 		"year" : "2011",
 		"filename" : "phil10fa11.pdf"
 	},
-
+	{
+		"title" : "Well-Being & its Political Significance",
+		"courseNum" : "267",
+		"courseCode" : "Philosophy",
+		"quarter" : "Spring",
+		"year" : "2015",
+		"filename" : "phil267sp15.pdf"
+	},
+	{
+		"title" : "Theories of Distributive Justice",
+		"courseNum" : "202",
+		"courseCode" : "Philosophy",
+		"quarter" : "Spring",
+		"year" : "2014",
+		"filename" : "na"
+	},
+	{
+		"title" : "Intro to Philosophy",
+		"courseNum" : "1",
+		"courseCode" : "Philosophy",
+		"quarter" : "Fall",
+		"year" : "2016",
+		"filename" : "phil1fa16.pdf"
+	},
+		{
+		"title" : "Intro to Social Theory",
+		"courseNum" : "12",
+		"courseCode" : "Philosophy",
+		"quarter" : "Winter",
+		"year" : "2004",
+		"filename" : "phil12wi04.pdf"
+	},
+	{
+		"title" : "Philosophy of Social Justice",
+		"courseNum" : "170",
+		"courseCode" : "Philosophy",
+		"quarter" : "Spring",
+		"year" : "2017",
+		"filename" : "na"
+	},
+	{
+		"title" : "Intro to Politics",
+		"courseNum" : "17",
+		"courseCode" : "Philosophy",
+		"quarter" : "Spring",
+		"year" : "2001",
+		"filename" : "na"
+	},
+		{
+		"title" : "Intro to Logic",
+		"courseNum" : "10",
+		"courseCode" : "Philosophy",
+		"quarter" : "Fall",
+		"year" : "2011",
+		"filename" : "phil10fa11.pdf"
+	},
+	{
+		"title" : "Well-Being & its Political Significance",
+		"courseNum" : "267",
+		"courseCode" : "Philosophy",
+		"quarter" : "Spring",
+		"year" : "2015",
+		"filename" : "phil267sp15.pdf"
+	},
+	{
+		"title" : "Theories of Distributive Justice",
+		"courseNum" : "202",
+		"courseCode" : "Philosophy",
+		"quarter" : "Spring",
+		"year" : "2014",
+		"filename" : "na"
+	},
+	{
+		"title" : "Intro to Philosophy",
+		"courseNum" : "1",
+		"courseCode" : "Philosophy",
+		"quarter" : "Fall",
+		"year" : "2016",
+		"filename" : "phil1fa16.pdf"
+	},
+		{
+		"title" : "Intro to Social Theory",
+		"courseNum" : "12",
+		"courseCode" : "Philosophy",
+		"quarter" : "Winter",
+		"year" : "2004",
+		"filename" : "phil12wi04.pdf"
+	},
+	{
+		"title" : "Philosophy of Social Justice",
+		"courseNum" : "170",
+		"courseCode" : "Philosophy",
+		"quarter" : "Spring",
+		"year" : "2017",
+		"filename" : "na"
+	},
+	{
+		"title" : "Intro to Politics",
+		"courseNum" : "17",
+		"courseCode" : "Philosophy",
+		"quarter" : "Spring",
+		"year" : "2001",
+		"filename" : "na"
+	},
+		{
+		"title" : "Intro to Logic",
+		"courseNum" : "10",
+		"courseCode" : "Philosophy",
+		"quarter" : "Fall",
+		"year" : "2011",
+		"filename" : "phil10fa11.pdf"
+	},
+	{
+		"title" : "Well-Being & its Political Significance",
+		"courseNum" : "267",
+		"courseCode" : "Philosophy",
+		"quarter" : "Spring",
+		"year" : "2015",
+		"filename" : "phil267sp15.pdf"
+	},
+	{
+		"title" : "Theories of Distributive Justice",
+		"courseNum" : "202",
+		"courseCode" : "Philosophy",
+		"quarter" : "Spring",
+		"year" : "2014",
+		"filename" : "na"
+	},
+	{
+		"title" : "Intro to Philosophy",
+		"courseNum" : "1",
+		"courseCode" : "Philosophy",
+		"quarter" : "Fall",
+		"year" : "2016",
+		"filename" : "phil1fa16.pdf"
+	},
+		{
+		"title" : "Intro to Social Theory",
+		"courseNum" : "12",
+		"courseCode" : "Philosophy",
+		"quarter" : "Winter",
+		"year" : "2004",
+		"filename" : "phil12wi04.pdf"
+	},
+	{
+		"title" : "Philosophy of Social Justice",
+		"courseNum" : "170",
+		"courseCode" : "Philosophy",
+		"quarter" : "Spring",
+		"year" : "2017",
+		"filename" : "na"
+	},
+	{
+		"title" : "Intro to Politics",
+		"courseNum" : "17",
+		"courseCode" : "Philosophy",
+		"quarter" : "Spring",
+		"year" : "2001",
+		"filename" : "na"
+	},
+		{
+		"title" : "Intro to Logic",
+		"courseNum" : "10",
+		"courseCode" : "Philosophy",
+		"quarter" : "Fall",
+		"year" : "2011",
+		"filename" : "phil10fa11.pdf"
+	},
 	{
 		"title" : "Ethical Theory",
 		"courseNum" : "160",
